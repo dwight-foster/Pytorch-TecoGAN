a6b3411d9a3301aadcb649cef06354525face2a5
Dwight foster <dwightfoster03@gmail.com>
2021-02-08 18:49:59 -0500

    This kind of works now.
    If you have a folder with videos it will turn them into images in the format the tecogan takes.

--- data/convert2images.py
+++ data/convert2images.py
@@ -1,14 +1,11 @@
 import torchvision
 import torch
 from torchvision import io, datasets, transforms, utils
-import cv2
 import PIL
 import os
 import argparse
-from converter import Converter
 from tqdm import tqdm
-
-conv = Converter()
+import cv2
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--VideoDir", type=str, help="Path to videos")
@@ -18,65 +15,55 @@ parser.add_argument("--keepshortvideos", default=True, type=bool, help="Keep ima
 parser.add_argument("--numframes", default=120, type=int, help="Should be one more than max_frm in main.py")
 args = parser.parse_args()
 
-videos = os.listdir(args.VideoDir)
-num_videos = len(videos)
+folders = os.listdir(args.VideoDir)
+num_folders = len(folders)
+for n in tqdm(range(num_folders)):
+    videos = os.listdir(args.VideoDir + folders[n])
+    num_videos = len(videos)
+    for i in range(num_videos):
 
+        if not os.path.isdir(args.VideoDir):
+            print("Input folder does not exist!")
+            break
+        if not os.path.isdir(args.OutputDir):
+            os.mkdir(args.OutputDir)
 
-def convert():
-    for i in tqdm(range(num_videos)):
+        path = args.VideoDir + folders[n] + "/" + videos[i]
+        cap = cv2.VideoCapture(path)
+        images = []
+        for k in range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
 
-        path = args.VideoDir + videos[i]
-        output = ".." + path.split(".")[2] + ".mp4"
-        if os.path.exists(output):
+            # Our operations on the frame come here
+            # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # Display the resulting frame
+            images.append(frame)
+
+        # When everything done, release the capture
+        cap.release()
+        if len(images) == 0:
+            print("here")
             continue
-        print(output)
-
-        convert = convert = conv.convert(path, output, {
-            'format': 'mp4',
-            'audio': {
-                'codec': 'aac',
-                'samplerate': 11025,
-                'channels': 2
-            },
-            'video': {
-                'codec': 'hevc',
-                'width': 720,
-                'height': 400,
-                'fps': 25
-            }})
-        for timecode in convert:
-            print(f"\rConverting ({timecode:.2f})...")
-
-
-# convert()
-
-for i in tqdm(range(num_videos)):
-    if not os.path.isdir(args.VideoDir):
-        print("Input folder does not exist!")
-        break
-    if not os.path.isdir(args.OutputDir):
-        os.mkdir(args.OutputDir)
-    path = args.VideoDir + videos[i]
-
-    path = args.VideoDir + videos[i]
-    images = io.read_video(path)[0]
-    if images.shape[0] < args.numframes:
-        for n in range(images.shape[0]):
-            folder_path = args.OutputDir + "scene_" + str(1000 + i)
-            if not os.path.isdir(folder_path):
-                os.mkdir(folder_path)
-            image = images[n]
-            image = image.permute(2, 0, 1)
-            image_path = folder_path + "/" + "col_high_" + str(n).zfill(4) + ".png"
-            utils.save_image(image.float(), fp=image_path)
-    else:
-        for n in range(args.numframes):
-            folder_path = args.OutputDir + "scene_" + str(1000 + i)
-            if not os.path.isdir(folder_path):
-                os.mkdir(folder_path)
-            image = images[n]
-            image = image.permute(2, 0, 1)
-            image_path = folder_path + "/" + "col_high_" + str(n).zfill(4) + '.png'
-            utils.save_image(image.float(), image_path)
-    if not args.keep_video:
-        os.remove(path)
+        elif len(images) < args.numframes:
+            for x in range(len(images)):
+                folder_path = args.OutputDir + "scene_" + str(1000 + i)
+                if not os.path.isdir(folder_path):
+                    os.mkdir(folder_path)
+                image = images[x]
+                if image is None:
+                    continue
+                image_path = folder_path + "/" + "col_high_" + str(x).zfill(4) + ".png"
+                cv2.imwrite(image_path, image)
+        else:
+            for x in range(args.numframes):
+                folder_path = args.OutputDir + "scene_" + str(1000 + i)
+                if not os.path.isdir(folder_path):
+                    os.mkdir(folder_path)
+                image = images[x]
+                image_path = folder_path + "/" + "col_high_" + str(x).zfill(4) + ".png"
+                if image is None:
+                    continue
+                cv2.imwrite(image_path, image)
+        if not args.keep_video:
+            os.remove(path)
